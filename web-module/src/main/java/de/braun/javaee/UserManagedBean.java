e327a0f465f3e131516ece21dc21858df1652712
Marco Braun <braun_marco@gmx.de>
2015-04-07 20:04:49 +0200

    add templates function

---
+++ web-module/src/main/java/de/braun/javaee/UserManagedBean.java
@@ -0,0 +1,61 @@
+package de.braun.javaee;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+import de.braun.javaee.models.User;
+import de.braun.javaee.service.UserManagerBeanRemote;
+
+@ManagedBean
+@SessionScoped
+public class UserManagedBean {
+	@EJB
+	private UserManagerBeanRemote umb;
+	
+	private User user;
+	private String surname;
+	private String name;
+		
+	public User getUser() {
+		
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String add () {
+		user = new User();
+		user.setName(this.name);
+		user.setSurname(this.surname);
+		umb.addUser(user);
+		
+		return "view_userlist";
+	}
+
+	public String update () {
+		umb.updateUser(user);
+		
+		return "view_userlist";
+	}	
+}
